00bda4c0ace3ce6938041ce32538e7e65cb594f4
Phil Roth <proth@endgame.com>
2015-01-26 21:59:41 -0500

    reorg and default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 var gulp        = require('gulp');
 var deploy      = require('gulp-gh-pages');
 
+var paths = {
+  scripts: ['./src/js/**/*.js'],
+  html: ['./src/index.html'],
+  dist: './dist/'
+};
+
+gulp.task('default', function(){
+  gulp.src(paths.scripts.concat(paths.html))
+    .pipe(gulp.dest(paths.dist));
+});
+
 /**
  * Push build to gh-pages
  */
