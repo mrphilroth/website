7740b33686c41950916491938abed97e4e2ecdfe
Phil Roth <proth@endgame.com>
2015-01-26 22:12:48 -0500

    adding bootstrap css

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,26 @@ var deploy      = require('gulp-gh-pages');
 
 var paths = {
   scripts: ['./src/js/**/*.js'],
+  style: ['./src/css/**/*.css'],
   html: ['./src/index.html'],
   dist: './dist/'
 };
 
-gulp.task('default', function(){
-  gulp.src(paths.scripts.concat(paths.html))
+gulp.task('default', ['html', 'styles']);
+
+// Copy html files
+gulp.task('html', function() {
+  return gulp.src(paths.scripts.concat(paths.html))
     .pipe(gulp.dest(paths.dist));
 });
 
-/**
- * Push build to gh-pages
- */
+// Copy css files
+gulp.task('styles', function() {
+  return gulp.src(paths.style)
+    .pipe(gulp.dest('./dist/css/'))
+});
+
+// Push build to gh-pages
 gulp.task('deploy', function () {
   return gulp.src("./dist/**/*")
     .pipe(deploy())
